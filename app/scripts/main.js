05ba012324c91b351a72788019482e910460736a
Emily Arnett <em_arnett@hotmail.com>
2014-01-14 23:42:47 +0800

    updated students

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,28 +24,83 @@ $( function() {
 			  image = "robyn.jpg";
 				name = "Robyn Steinberg";
 				country = "USA";
-				text = '"Robyn Stuff"'
+				text = ''
 				github = "https://github.com/robss04";
 				linkedin = "http://hk.linkedin.com/in/robynsteinberg";
 				break;
-			// case "Karen":
-			// 	break;
-			// case "Josiah":
-			// 	break;
-			// case "Deepak":
-			// 	break;
-			// case "Kingsley":
-			// 	break;
-			// case "Ian":
-			// 	break;
-			// case "Sophia":
-			// 	break;
-			// case "Steph":
-			// 	break;
-			// case "Bruno":
-			// 	break;
-			// case "Jin":
-			// 	break;
+			case "Bruno":
+			  image = "bruno.jpg";
+				name = "Bruno Wu";
+				country = "USA/HK";
+				text = '"I had a prior career in finance and now am transitioning into the software development field. Seeking to start the software apprenticeship journey by joining an innovative (and preferably industry disrupting) company. Areas of interest include: Back-end dev, data science, machine learning, and analytics."'
+				github = "https://github.com/wubr2000";
+				linkedin = "http://www.linkedin.com/in/josiahhatch";
+				break;
+			case "Josiah":
+			  image = "gFMNHyA.gif"
+			  name = "Josiah Hatch"
+			  country = "USA"
+			  text = '"After looking for a job in sales for a few months, I stumbled upon the WDI course. I\'ve always had an interest in technology, but I\'ve never had the chance to learn. I\'m so happy that I took the time to learn how to code, I absolutely love it and wish I had learned years ago!"'
+				github = "https://github.com/vbsiqebu";
+				linkedin = "http://hk.linkedin.com/in/brunowu";
+				break;
+			case "Jin":
+			  image = "jin.jpg"
+			  name = "Jin Hyejin Yang"
+			  country = "Korea"
+			  text = '"I am excited to work as web developer as I believe in the power of changing the world. With international experience in science, banking and fashion, I adapt and focus on problem solving."'
+				github = "https://github.com/yytina"
+				linkedin = "http://hk.linkedin.com/in/jinyy"
+				break;
+			case "Deepak":
+			  image = "person.jpg"
+			  name = "Deepak Jayakumaran"
+			  country = "Singapore"
+			  text = ''
+				github = "https://github.com/godelian"
+				linkedin = "#"
+				break;
+			case "Karen":
+				image = "person.jpg"
+				name ="Karen Farzam"
+				country = "France"
+				text = ''
+				github = "https://github.com/KarenZam"
+				linkedin = "http://hk.linkedin.com/pub/karen-farzam/0/ab7/2a"
+				break;
+			case "Kingsley":
+			  image = "person.jpg"
+				name ="Kingsley Man"
+				country = "UK/HK"
+				text = ''
+				github = "https://github.com/kingsleyman"
+				linkedin = "http://hk.linkedin.com/in/kingsleyman"
+				break;
+			case "Ian":
+			  image = "person.jpg"
+				name ="Ian O'Connor"
+				country = "UK"
+				text = ''
+				github = "https://github.com/Xenos54"
+				linkedin = "http://hk.linkedin.com/in/ianoconnorcqf"
+				break;
+			case "Sophia":
+			  image = "person.jpg"
+				name ="Sophia Raji"
+				country = "Morocco"
+				text = ''
+				github = "https://github.com/sudosoph"
+				linkedin = "#"
+				break;
+			case "Steph":
+			  image = "person.jpg"
+				name ="Stephanie Siaw"
+				country = "Indonesia"
+				text = ''
+				github = "https://github.com/stephs829/"
+				linkedin = "http://hk.linkedin.com/in/stephaniesiaw/en"
+				break;
+
 		};
 		$('#modal-name').text(name);
 		$("#modal-image").html('<img id="myModalImage" src="images/' + image + '" alt="student image">');
